b080eebf327a985a93796034e2fca0d57aa89234
Waseem khalil <waseemkhalilworl@gmail.com>
2024-07-01 13:11:45 -0700

    42% work done

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const column = document.querySelectorAll(".column");
 const addCardForm = document.querySelector("#addCardForm");
+const tasked = document.querySelectorAll(".task1")
 const addTask = (event) => {
   event.preventDefault();
 
   const currentForm = event.target; 
   const value = currentForm.elements[0].value; 
-  const parent = currentForm.parentElement; 
+  const parent = currentForm.parentElement;
+  // console.log(parent); 
   const ticket = createTicket(value); 
 
   parent.insertBefore(ticket, currentForm); // add new task before form
+  currentForm.reset(); 
 
-  currentForm.reset(); // clearing form
+  // const h3Value = parent.children[0].innerText;
+  // // console.log(h3Value);
+  // if (!Array.isArray(savedTasks[h3Value])) {
+  //   savedTasks[h3Value] = [];
+  // }
+  
+  // savedTasks[h3Value].push(value);
+
+  // localStorage.setItem("Name", JSON.stringify(savedTasks)); 
+
 };
 
 for (let i = 0; i < column.length; i++) {
