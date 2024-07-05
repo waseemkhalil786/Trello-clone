9d32a672aee915f2df3fcfa0d7fccf4e9d067162
Waseem khalil <waseemkhalilworl@gmail.com>
2024-07-05 13:42:53 -0700

    45% work done

--- app.js
+++ app.js
@@ -1,43 +1,124 @@
-const column = document.querySelectorAll(".column");
+const main = document.querySelector("#main");
 const addCardForm = document.querySelector("#addCardForm");
-const tasked = document.querySelectorAll(".task1")
+const btn = document.querySelector("#addCard");
+// console.log(main);
+
+let jiselementkohamnepkrahuah = null;
+
+btn.addEventListener("click", () => {
+  const divName = prompt("Plese enter CardName!!");
+
+  const mainDiv = createCard(divName);
+  main.appendChild(mainDiv);
+  btn.before(mainDiv);
+});
+
 const addTask = (event) => {
   event.preventDefault();
-
-  const currentForm = event.target; 
-  const value = currentForm.elements[0].value; 
+  const currentForm = event.target;
+  const value = currentForm.elements[0].value;
   const parent = currentForm.parentElement;
-  // console.log(parent); 
-  const ticket = createTicket(value); 
+  // console.log(parent);
+  const ticket = createTicket(value);
 
-  parent.insertBefore(ticket, currentForm); // add new task before form
-  currentForm.reset(); 
+  parent.insertBefore(ticket, currentForm);
+  currentForm.reset();
 
-  // const h3Value = parent.children[0].innerText;
-  // // console.log(h3Value);
-  // if (!Array.isArray(savedTasks[h3Value])) {
-  //   savedTasks[h3Value] = [];
-  // }
-  
-  // savedTasks[h3Value].push(value);
+  //   const h3Value = parent.children[0].innerText;
+  //   // console.log(h3Value);
+  //   if (!Array.isArray(savedTasks[h3Value])) {
+  //     savedTasks[h3Value] = [];
+  //   }
 
-  // localStorage.setItem("Name", JSON.stringify(savedTasks)); 
+  //   savedTasks[h3Value].push(value);
 
+  //   localStorage.setItem("savedTasks", JSON.stringify(savedTasks))
+  //   currentForm.reset();
 };
 
-for (let i = 0; i < column.length; i++) {
-  const form = column[i].lastElementChild; // selecting every column's form because form is last element
+for (let i = 0; i < main.length; i++) {
+  const form = main[i].lastElementChild;
 
   form.addEventListener("submit", addTask);
 }
 
+//local storge practice
+
+// let savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
+
+// if (!savedTasks) {
+//   savedTasks = {};
+// }
+
+// for (const title in savedTasks) {
+//   const card = createCard(title);
+
+//   const arrayOfTasks = savedTasks[title];
+
+//   for (let i = 0; i < arrayOfTasks.length; i++) {
+//     const p = createTicket(arrayOfTasks[i]);
+
+//     card.insertBefore(p, card.lastElementChild);
+//   }
+
+//   main.insertBefore(card, btn);
+// }
+
+const createCard = (waseem) => {
+  const mydiv = document.createElement("div");
+  const h3 = document.createElement("h3");
+  const form = document.createElement("form");
+  const myinput = document.createElement("input");
+  const h3Text = document.createTextNode(waseem);
+
+  mydiv.setAttribute("class", "main");
+  myinput.setAttribute("class", "task1");
+  myinput.setAttribute("type", "text");
+  myinput.setAttribute("placeholder", "Add task");
+
+  h3.appendChild(h3Text);
+  mydiv.appendChild(h3);
+  form.appendChild(myinput);
+  mydiv.appendChild(form);
+
+  mydiv.addEventListener("dragleave", (event) => event.preventDefault());
+  mydiv.addEventListener("dragover", (event) => event.preventDefault());
+
+  mydiv.addEventListener("drop", (event) => {
+    const jispedropkiya = event.target;
+    if (jispedropkiya.className.includes("main")) {
+      jispedropkiya.appendChild(jiselementkohamnepkrahuah);
+    }
+
+    if (jispedropkiya.className.includes("ticket")) {
+      jispedropkiya.parentElement.appendChild(jiselementkohamnepkrahuah);
+    }
+  });
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const inputKiValue = event.target.firstChild.value;
+    console.log(inputKiValue);
+
+    const para = createTicket(inputKiValue);
+    form.before(para);
+    // console.dir(event.target);
+    form.reset();
+  });
+  return mydiv;
+};
+
 const createTicket = (value) => {
-  //
   const ticket = document.createElement("p");
   const elementText = document.createTextNode(value);
 
   ticket.setAttribute("draggable", "true");
   ticket.appendChild(elementText);
+
+  ticket.addEventListener("mousedown", (event) => {
+    jiselementkohamnepkrahuah = event.target;
+    console.log("chal gia");
+  });
 
   return ticket;
 };
